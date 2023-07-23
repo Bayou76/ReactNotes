37b5bb9d420216559e911d3dfefbc17ed1ff122c
Bayou76 <103516275+Bayou76@users.noreply.github.com>
2023-07-23 02:24:52 +0200

    noteList

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,4 +1,11 @@
 import s from "./style.module.css";
+import { NoteList } from "components/NoteList/NoteList";
+import { TextCard } from "components/TextCard/TextCard";
+
 export function NoteBrowse(props) {
-  return <>Note browse</>;
+  return (
+    <>
+      <NoteList />
+    </>
+  );
 }
